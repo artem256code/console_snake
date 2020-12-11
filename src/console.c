fed4bf18dc1a01da7ca191730e604cada5df3812
artem256code <artem_code@outlook.com>
2020-12-11 23:02:16 +0300

    Added 'render' and 'clear' functions

--- src/console.c
+++ src/console.c
@@ -1,6 +1,7 @@
 #include <malloc.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "console.h"
 
@@ -27,11 +28,29 @@ struct Console* initConsole(struct Console *this){
     }
     this->buffer = buffer;
     this->addToBuffer = _addToBuffer;
-
+    this->clear = _clear;
+    this->render = _render;
     return this;
 }
 
 
 static void _addToBuffer(struct Console *this, int row, int col, char sym){
     this->buffer[row][col] = sym;
+}
+
+static void _clear(Console *this){
+    system("clear");
+    for(int i = 0; i < this->rows; i++){
+        for(int j = 0; j < this->columns; i++){
+            this->buffer[i][j] = ' ';
+        }
+    }
+}
+
+static void _render(Console *this){
+    for(int i = 0; i < this->rows; i++){
+        for(int j = 0; j < this->columns; j++){
+            putchar(this->buffer[i][j]);
+        }
+    }
 }
--- src/console.h
+++ src/console.h
@@ -7,15 +7,17 @@ typedef struct Console{
     char **buffer;      // Buffer that stores the playing field
 
     void (*addToBuffer) (struct Console *this, int row, int col, char sym);
-
+    void (*clear)  (struct Console *this);
+    void (*render) (struct Console *this);
 } Console;
+
 
 /**
  * @brief Create a console object
  * @param this - pointer to the console object
  * @return       pointer to the console object
  */
-struct Console* initConsole(struct Console *this);
+struct Console* initConsole(Console *this);
 
 
 /**
@@ -25,7 +27,20 @@ struct Console* initConsole(struct Console *this);
  * @param col   - column in the buffer
  * @param sym   - the symbol to be added
  */
-static void _addToBuffer(struct Console *this, int row, int col, char sym);
+static void _addToBuffer(Console *this, int row, int col, char sym);
 
+
+/**
+ * @brief Clear the console field
+ * @param this  - pointer to the console object
+ */
+static void _clear(Console *this);
+
+
+/**
+ * @brief _render
+ * @param this
+ */
+static void _render(Console *this);
 
 #endif
